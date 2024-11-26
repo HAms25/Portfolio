73815a198c853105d9e5b239db377fcec20dd88c
HAms25 <hilari.martinez@tecsup.edu.pe>
2024-11-25 23:09:29 -0500

    Reorganized items :twisted_rightwards_arrows:

--- src/data/dataProject.tsx
+++ src/data/dataProject.tsx
@@ -2,19 +2,19 @@ import { Project } from "@/pages/Project/index"
 
 export const dataProject: Project[] = [
     {
-        title: "SafeHaven",
-        description: "SafeHaven ofrece servicios convenientes de mantenimiento y reparación a domicilio para electrodomésticos y equipos del hogar.",
-        image: "https://res.cloudinary.com/dovqxavyy/image/upload/v1730871760/vwku1nldaxkaz2xsmpjq.png" ,
-        deploy: null,
-        github: "https://github.com/emvmart23/Frontend-Safehaven.git",
-        tech: ["React JS", "Vite JS", "TailwindCSS", "Redux"]
-    },
-    {
         title: "Apertura Porte",
         description: "SafeHaven ofrece servicios convenientes de mantenimiento y reparación a domicilio para electrodomésticos y equipos del hogar.",
         image: "https://res.cloudinary.com/dovqxavyy/image/upload/v1730871455/i06fxwiuhsssbft3qhmf.png" ,
         deploy: "https://apertura-porte.vercel.app/",
         github: "https://github.com/HAms25/Cerrajeria-Martinez.git",
         tech: ["React JS", "Vite JS", "TailwindCSS"]
+    },
+    {
+        title: "SafeHaven",
+        description: "SafeHaven ofrece servicios convenientes de mantenimiento y reparación a domicilio para electrodomésticos y equipos del hogar.",
+        image: "https://res.cloudinary.com/dovqxavyy/image/upload/v1730871760/vwku1nldaxkaz2xsmpjq.png" ,
+        deploy: null,
+        github: "https://github.com/emvmart23/Frontend-Safehaven.git",
+        tech: ["React JS", "Vite JS", "TailwindCSS", "Redux"]
     },
 ]
