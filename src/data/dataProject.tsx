2cbd77f384b39e88e0cd43b5cd328c866eb00d95
HAms25 <hilari.martinez@tecsup.edu.pe>
2024-12-13 23:40:25 -0500

    Add project :heavy_plus_sign:

--- src/data/dataProject.tsx
+++ src/data/dataProject.tsx
@@ -10,6 +10,14 @@ export const dataProject: Project[] = [
         tech: ["React JS", "Vite JS", "TailwindCSS"]
     },
     {
+        title: "Rick and Morty",
+        description: "Este proyecto es una aplicación interactiva que consume datos de la API de Rick and Morty para mostrar información sobre los personajes de la serie de forma dinámica y visualmente atractiva.",
+        image: "https://res.cloudinary.com/dovqxavyy/image/upload/v1734129239/rickandmorty_qt9lpy.png" ,
+        deploy: "https://rick-and-morty-api-two-sigma.vercel.app/",
+        github: "https://github.com/HAms25/RickAndMortyAPI.git",
+        tech: ["Next JS", "React JS", "TailwindCSS"]
+    },
+    {
         title: "SafeHaven",
         description: "SafeHaven ofrece servicios convenientes de mantenimiento y reparación a domicilio para electrodomésticos y equipos del hogar.",
         image: "https://res.cloudinary.com/dovqxavyy/image/upload/v1730871760/vwku1nldaxkaz2xsmpjq.png" ,
